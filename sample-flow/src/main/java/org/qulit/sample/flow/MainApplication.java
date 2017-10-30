6aa68fcada7db0eccb32087a9d8fa1531cbab000
Qulei <qulei@le.com>
2017-10-30 16:33:54 +0800

    add the basic url format.
    Signed-off-by: qulit <267023750@qq.com>

---
+++ sample-flow/src/main/java/org/qulit/sample/flow/MainApplication.java
@@ -0,0 +1,13 @@
+package org.qulit.sample.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
